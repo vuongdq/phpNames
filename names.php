82283a6b1baa207ba07a7d43fd324d81cebf530b
vuongdq <quocvuong_it@yahoo.com>
2022-08-19 17:40:59 +0700

    init project

---
+++ names.php
@@ -0,0 +1,12 @@
+<?php
+include ("simple_html_dom.php");
+$baseUrl = "https://www.names.org/baby-names-by-letter/a/";
+$domain = "https://www.names.org";
+
+$html = file_get_html($baseUrl);
+$pageNums= $html->find("ul.pagination.pagination-lg",0)->find("li");
+foreach ($pageNums as $pagenum){
+    $pageResultNum = $domain.$pagenum->find("a",0)->href;
+    echo $pageResultNum;
+    echo "<br />";
+}
