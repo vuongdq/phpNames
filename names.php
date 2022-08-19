4c18be76f370d26b282bf2df8bbbc1a671d3d37a
vuongdq <quocvuong_it@yahoo.com>
2022-08-19 17:57:45 +0700

    Update

--- names.php
+++ names.php
@@ -7,9 +7,14 @@ $html = file_get_html($baseUrl);
 $pageNums= $html->find("ul.pagination.pagination-lg",0)->find("li");
 foreach ($pageNums as $pagenum){
     $pageResultNum = $domain.$pagenum->find("a",0)->href;
-    echo $pageResultNum;
+    echo "-----------------".$pageResultNum."........";
     echo "<br />";
     $html2 = file_get_html($pageResultNum);
     $nameLink = $html2->find("table.table",0)->find("tbody",0)->find("tr");
-    var_dump($nameLink);
+    foreach ($nameLink as $link){
+        echo $aboutLink = $domain.$link->find("a",0)->href;
+        echo "<br />";
+    }
+
 }
+
